70fcd61a1ea646eeb32fd5b36600c59ef29591b9
Amit7831 <amitranjanmalik21@gmail.com>
2025-11-10 11:44:33 +0530

    task 7 complete

---
+++ Day-8/first.js
@@ -0,0 +1,16 @@
+console.log("Hello welcome to GIET Ghangapatna");
+
+//Bariables
+//rules of variable declaration
+//rule-1"
+  // only digits,character,_,$ we can use
+//rule 2:
+  // Variables are case sensitive
+// rule 3:
+    // The first letter should not be digit
+// rule 4:
+    // reserved key word are not possible for declare a varible     
+
+    
+a = 20;
+console.log(a);
