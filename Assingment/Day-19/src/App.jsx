692394b9d1f3717d4d5c2e6926cf31e1ee030ecf
Amit7831 <amitranjanmalik21@gmail.com>
2026-01-12 21:59:36 +0530

    Done

---
+++ Assingment/Day-19/src/App.jsx
@@ -0,0 +1,22 @@
+
+import About from './About'
+import './App.css'
+import ContactUs from './ContactUs'
+import Home from './Home'
+import Nav from './Nav'
+import Service from './Service'
+
+function App() {
+  
+  return (
+    <>
+      <Nav/>
+      <Home />
+      <About />
+      <Service />
+      <ContactUs/>
+    </>
+  )
+}
+
+export default App
